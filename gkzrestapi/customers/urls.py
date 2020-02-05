451fce5efae6ab63e29d4312de75ff4893d12a92
Sivabudh Umpudh <sivabudh.u@codium.co>
2020-02-05 23:44:04 +0700

    Django is done

--- gkzrestapi/customers/urls.py
+++ gkzrestapi/customers/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.customer_list, name='list'),
     path('<int:pk>/', views.customer_detail, name='detail'),
-    path('<int:age>/age/', views.customer_list_age, name='age'),
+    path('age/<int:age>/', views.customer_list_age, name='age'),
 ]
